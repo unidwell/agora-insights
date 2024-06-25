0f7d22db10d79b87218f02ae3debe6b3d7c7a6cc
unidwell <gregor.mirai@gmail.com>
2024-06-25 16:19:01 +0200

    feat: add poll's total and event's total to simplify the calculation and provide more incremental price calculations.

--- src/prediction-market.js
+++ src/prediction-market.js
@@ -52,12 +52,22 @@ export default class PredictionMarket {
 
     market.participants.add(participant)
 
+    // Add a bet to the specified pool.
     if (!market.pools[outcomeName]) {
       market.pools[outcomeName] = []
     }
 
     const bets = market.pools[outcomeName]
     bets.push([participant, amount])
+
+    // Update the total amount of money bet on specific outcome.
+    if (!market.poolsTotal[outcomeName]) {
+      market.poolsTotal[outcomeName] = 0
+    }
+    market.poolsTotal[outcomeName] += amount
+
+    // Update the total amount of money bet on the event.
+    market.total += amount
   }
 
   // resolveMarket(eventName, winningOutcomeName)
@@ -82,11 +92,9 @@ export default class PredictionMarket {
 
     const pools = market.pools
     const betValues = flatten(Object.values(pools)).map(([_, amount]) => amount)
-    const totalAmount = betValues.reduce((total, amount) => total + amount, 0)
-
-    const rewards = {}
 
     // Participants keep the correctly places bets.
+    const rewards = {}
     forEach(pools, (bets, outcomeName) => {
       bets.forEach(([participant, amount]) => {
         if (rewards[participant] === undefined) {
@@ -99,20 +107,37 @@ export default class PredictionMarket {
     })
 
     // The total amount of bets for the winning outcome.
-    const totalAmountOnWinningOutcome = pools[winningOutcomeName].reduce(
-      (total, [_, amount]) => total + amount,
-      0
-    )
+    const winningPoolTotal = market.poolsTotal[winningOutcomeName]
 
-    // calculate the rest of the money
-    const restOfTheMoney = totalAmount - totalAmountOnWinningOutcome
+    // The money to be distributed among the winners.
+    const distributeAmount = market.total - winningPoolTotal
 
-    // distribute the rest of the money among the winners proportionally to the amount of money they bet
+    // Distribute the the money among the winners proportionally to the amount they have betted.
     pools[winningOutcomeName].forEach(([participant, amount]) => {
-      rewards[participant] += (amount / totalAmountOnWinningOutcome) * restOfTheMoney
+      rewards[participant] += (amount / winningPoolTotal) * distributeAmount
     })
 
-    return { rewards, totalAmount }
+    return { rewards, totalAmount: market.total }
+  }
+
+  // getCurrentOutcomePrice(eventName, outcomeName)
+  // @param {string} eventName - the name of the event.
+  // @param {string} outcomeName - the name of the outcome.
+  //
+  // Calculate the current price of the outcome as the ratio of the total amount of money bet on the outcome
+  // and the total amount of money bet on the event.
+  getCurrentOutcomePrice(eventName, outcomeName) {
+    const market = this.market[eventName]
+    assert.ok(market !== undefined, new Error(`Event '${eventName}' does not exist.`))
+    assert.ok(
+      market.outcomes.has(outcomeName) !== undefined,
+      new Error(`Outcome '${outcomeName}' does not exist for event '${eventName}'.`)
+    )
+
+    const total = market.total
+    const totalOutcome = market.poolsTotal[outcomeName]
+
+    return totalOutcome / total
   }
 
   getMarket() {
